3e362628178d3c20b26551108624bd13bdb4d8cb
Yuto Watanabe <yuto.w51942@gmail.com>
2021-10-26 17:20:00 +0900

    changed handler

--- handler/handler.go
+++ handler/handler.go
@@ -7,7 +7,8 @@ import (
 	"github.com/yuto51942/access-tracker/control"
 )
 
-func RootHandler(w http.ResponseWriter, r *http.Request) {
+// Tracking and redirect
+func TrackHandler(w http.ResponseWriter, r *http.Request) {
 	// get url path.
 	// Example: http://example.com/hoge -> hoge
 	path := strings.FieldsFunc(r.URL.Path, func(r rune) bool {
@@ -23,8 +24,26 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(id))
 }
 
-func TrackHandler(w http.ResponseWriter, r *http.Request) {
-	bytes, err := control.Create()
+// Setting: Create url, reference access history and delete tracking url.
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		WhoisHandler(w, r)
+	case http.MethodPost:
+		CreateHandler(w, r)
+	case http.MethodDelete:
+		DeleteHandler(w, r)
+	}
+}
+
+// Create tracking url
+func CreateHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	// TODO
+	redirectUrl := ""
+
+	bytes, err := control.Create(&ctx, redirectUrl)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -34,6 +53,22 @@ func TrackHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(bytes)
 }
 
+// Delete tracking url
+func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	id := ""
+	accessKey := ""
+
+	if err := control.Delete(&ctx, id, accessKey); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+// Reference access history.
 func WhoisHandler(w http.ResponseWriter, r *http.Request) {
 
 }
