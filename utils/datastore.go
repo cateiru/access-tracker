26ddba1fb2131e0df00c05e21e3c4c0f4d04303a
Yuto Watanabe <yuto.w51942@gmail.com>
2021-10-26 19:38:54 +0900

    fixed a bug

--- utils/datastore.go
+++ utils/datastore.go
@@ -5,6 +5,10 @@ import "cloud.google.com/go/datastore"
 func CreateKey(kind string, keys ...string) *datastore.Key {
 	var key = new(datastore.Key)
 
+	if len(keys) == 0 {
+		return datastore.IncompleteKey(kind, nil)
+	}
+
 	for _, keyId := range keys {
 		key = datastore.NameKey(kind, keyId, key)
 	}
